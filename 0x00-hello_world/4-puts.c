2dc9fb68441a2aa9e909025ed765a8237d070bd8
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-06-30 16:22:40 -0700

    a c program that prints exact words

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/** * main - starts execution
- * return - 0 if executed properly
- */
+
+/**
+*main - Entry point
+*Return: 0 if exited properly
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

ae1aa5865be342b30e91f0917fdba5246a958372
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-06-30 16:35:14 -0700

    a c that prints an output usinf printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Return: 0 if exited properly
+ */
+int main (void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

f680288b32d2868f7e163a9a82d100c3a9bd926d
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-06-30 15:54:21 -0700

    a c program that prints a specific output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/** * main - starts execution
+ * return - 0 if executed properly
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}

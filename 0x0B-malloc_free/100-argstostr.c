5fddae27fdb2cdb10059536fea8ef685f822e761
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-07-26 10:15:31 -0700

    fixed a malloc error

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,36 +10,30 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len;
-	char *str;
+	int i, j, len, bufferlen;
+	char *p;
 
-	if (ac == 0 || av == NULL)
+	if (ac == 0)
+		return (NULL);
+	if (av == NULL)
 		return (NULL);
 
-	for (i = 0; i < ac; i++)
+	i = j = len = bufferlen = 0;
+	for (i = 0; av[i]; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 			len++;
-		len++;
 	}
-
-	str = malloc(sizeof(char) * (len + 1));
-
-	if (str == NULL)
+	p = (char *)malloc(len * sizeof(char) + ac + 1);
+	if (p == NULL)
 		return (NULL);
-
-	k = 0;
-
-	for (i = 0; i < ac; i++)
+	for (i = 0; av[i]; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			str[k] = av[i][j];
-			k++;
-		}
-		str[k] = '\n';
-		k++;
+		for (j = 0; av[i][j]; j++, bufferlen++)
+			p[bufferlen] = av[i][j];
+		p[bufferlen] = '\n';
+		bufferlen++;
 	}
-
-	return (str);
+	p[bufferlen] = '\0';
+	return (p);
 }

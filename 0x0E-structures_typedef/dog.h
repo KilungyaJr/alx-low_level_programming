20134da16090d26c9f42c48a6dcc63e5a4c58b2e
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-08-01 05:33:19 -0700

    header file for the dog structure tasks

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog'd profile
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* #ifndef DOG_H */

92535eb9800450f404a4f32b6e80d414886d9037
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-08-04 05:04:48 -0700

    updated main header file with structure for task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,14 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+
+typedef struct validTypes
+{
+	char *valid;
+	void (*f)();
+} v_types;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);

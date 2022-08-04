df88eb660f906e951b5932a74fb97d392c0714d3
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-08-04 05:25:19 -0700

    updated header file structure description

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,14 +1,23 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' \
+		|| format[i] == 'f' || format[i] == 's')
 
+/**
+ * struct validTypes - structure
+ * @valid: element
+ * @f: function
+ *
+ * Description: types of arguments passed to function
+ */
 typedef struct validTypes
 {
 	char *valid;
 	void (*f)();
 } v_types;
 
+/* function prototypes */
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);

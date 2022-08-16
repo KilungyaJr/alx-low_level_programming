6811a2984c41d13702c36cdb11c4e5be225ed4c0
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-08-17 00:15:15 +0300

    fixed some code fails

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,6 +8,18 @@
   */
 listint_t *reverse_listint(listint_t **head)
 {
-	head++;
-	return (0);
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+
+	return (*head);
 }

f4c7b1d93e7b2d80b00ba6dee70c521583592042
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-08-17 00:47:06 +0300

    basic - function that finds the loop in a linked list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+  * find_listint_loop - finds the loop in a linked list dsncks
+  * @head:
+  *
+  * Return: The address of the node where the loop starts,
+  * NULL if there is no loop
+  */
+listint_t *find_listint_loop(listint_t *head)
+{
+	head++;
+	return (0);
+}

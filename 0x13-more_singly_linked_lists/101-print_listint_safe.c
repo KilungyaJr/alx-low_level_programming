6339e84e4dd5620e14f6e4c88a7fc59b8bee701b
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-08-17 00:48:23 +0300

    fixed some betty fails

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -22,7 +22,8 @@ size_t print_listint_safe(const listint_t *head)
 			head = head->next;
 		else
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			printf("-> [%p] %d\n", (void *)head->next,
+			       head->next->n);
 			break;
 		}
 	}

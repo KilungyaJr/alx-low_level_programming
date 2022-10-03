ee52e0fa9d75555987162a62cff581d2708b5994
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-10-03 16:38:52 +0300

    c functions source file for task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,39 @@
+/**
+ * add - sums a and b
+ */
+int add(int a, int b)
+{
+	return a + b;
+}
+
+/**
+ * sub - subtracts b from a
+ */
+int sub(int a, int b)
+{
+	return a - b;
+}
+
+/**
+ * mul - multiplies a and b
+ */
+int mul(int a, int b)
+{
+	return a * b;
+}
+
+/**
+ * div - divides a and b
+ */
+float div(float a, float b)
+{
+	return a / b;
+}
+
+/**
+ * mod - returns a modulo absolute value of b
+ */
+int mod(int a, int b)
+{
+	return a % b;
+}
